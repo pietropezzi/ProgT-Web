6cc2dbe16be4e19a3cc00a2e903bedccbf60d73f
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-12-19 12:30:20 +0100

    Gestione sessione con logout

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once("db/database.php");
 require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "pc_hardware", 3306);
--- template/user-info.php
+++ template/user-info.php
@@ -1,3 +1,4 @@
 <h3>Attualmente connesso come:</h3>
 <h2><?php echo $_SESSION["nome"]; ?></h2>
 <h4><?php echo $_SESSION["email"]; ?></h4>
+<a href="template/logout.php">Logout</a>
