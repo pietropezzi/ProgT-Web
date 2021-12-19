5d23da713f95a60dfc7cf41f60ea286b2c1e4615
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-12-19 16:21:18 +0100

    add css to register

--- template/register-form.php
+++ template/register-form.php
@@ -1,35 +1,35 @@
-<div class="bg-ligt  text-center  m-3 p-3 ">
-    <label for="username">Registrati</label>
+<div class="my-4 text-white register">
+    <label class=" mb-2" for="username">Registrati</label>
     <div class="">
         <form name="registrazione" action="registrazione.php" onsubmit="return validateForm()" method="post"action="#">
             <div class="user-details">
                 <div class="input-box">
-                    <label for="username">Nome</label>
-                    <input name = "name" type="text" placeholder="Enter your name" required>
+                    <label class="mb-1" for="username">Nome</label><br>
+                    <input name="name" type="text" placeholder="Enter your name" required>
                 </div>
                 <div class="input-box">
-                    <label for="username">Username</label>
-                    <input name = "username" type="text" placeholder="Enter your username" required>
+                    <label class="mb-1" for="username">Username</label><br>
+                    <input name="username" type="text" placeholder="Enter your username" required>
                 </div>
                 <div class="input-box">
-                    <label for="username">Email</label>
-                    <input name = "email" type="text" placeholder="Enter your email" required>
+                    <label class="mb-1" for="username">E-mail</label><br>
+                    <input name="email" type="text" placeholder="Enter your email" required>
                 </div>
                 <div class="input-box">
-                    <label for="username">Numero di telefono</label>
-                    <input name = "phone" type="text" placeholder="Enter your number" required>
+                    <label class="mb-1" for="username">Numero di telefono</label><br>
+                    <input name="phone" type="text" placeholder="Enter your number" required>
                 </div>
                 <div class="input-box">
-                    <label for="username">Password</label>
-                    <input name = "password" type="password" placeholder="Enter your password" required>
+                    <label class="mb-1" for="username">Password</label><br>
+                    <input name="password" type="password" placeholder="Enter your password" required>
                 </div>
                 <div class="input-box">
-                    <label for="username">Conferma Password</label>
-                    <input name = "password2" type="password" placeholder="Confirm your password" required>
+                    <label class="mb-1" for="username">Verifica Password</label><br>
+                    <input name="password2" type="password" placeholder="Confirm your password" required>
                 </div>
             </div>
-            <div class="button">
-                <input type="submit" value="Registrati">
+            <div>
+                <input class="button text-white" type="submit" value="Registrati">
             </div>
         </form>
     </div>
