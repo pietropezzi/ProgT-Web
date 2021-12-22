e0e5b0d6d696ffe1ffc3741807d91bc54f8fc790
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-12-22 17:01:22 +0100

    Added footer versione di prova, da fixare un po

--- template/footer.php
+++ template/footer.php
@@ -1,1 +1,24 @@
-<h2> Footer </h2>
+<footer>
+    <div class="about-contatti">
+        <h3>About & Contatti</h3>
+        <p>pchardware è un piccolo sito per acquisti e vendite di parti hardware per pc.<br>
+        Il sito è attualmente gestito da:</p>
+        <ul class="gestori">
+            <li>Fabio Chiarini</li>
+            <li>Pietro Pezzi</li>
+        </ul>
+        <p>Per informazioni e assistenza contattare uno dei gestori.</p>
+    </div>
+    <div class="ext-links">
+        <h3>Links</h3>
+        <ul class="linklist">
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Linkedin</a></li>
+        </ul>
+    </div>
+    <hr/>
+    <div class="bottom-text">
+        <p> PCHARDWARE.COM - 2021/2022 </p>
+    </div>
+</footer>
