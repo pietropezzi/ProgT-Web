e850a196b6cb82aebcf1a4d4d89ba21694671a11
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-12-27 17:06:26 +0100

    Add files via upload

--- js/checkRegisterForm.js
+++ js/checkRegisterForm.js
@@ -1,16 +1,49 @@
 function checkRegisterForm() {
+	
+	var name = document.getElementById("name").value
+	var username = document.getElementById("username").value
+	var email = document.getElementById("email").value	
+	var phone = document.getElementById("phone").value	
+	var psw = document.getElementById("psw").value
+	var psw2 = document.getElementById("psw2").value		
+	
+	//test
+	var number = /^[0-9]+$/;
+	var letters = /^[A-Za-z]+$/;
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-
-  var x = document.forms["registrazione"]["email"].value;
-  if (x.indexOf("@") == -1 ) {
-    alert("Attenzione: L'email e sbagliata");
-    return false;
-  }    
-  
-  var psw = document.getElementById("psw").value
-  var psw2 = document.getElementById("psw2").value
-  if (psw != psw2) {
-    alert("Attenzione: Le due password sono diverse.");
-    return false;
+	if(!letters.test(name)){
+		alert('Attenzione: Il nome deve contenere solo lettere dell\'alfabeto');
+		return false;
 	}
+		
+	if(!letters.test(username)){
+		alert('Attenzione: L\' username deve contenere solo lettere dell\'alfabeto');
+		return false;
+	}	
+	
+	if (!filter.test(email)){
+		alert('Attenzione: Email non valida');
+		return false;
+	}
+		
+	if (!number.test(phone)){
+		alert('Attenzione: Il numero di telefono deve contenere solo numeri');
+		return false;
+	}
+	
+	if(psw != psw2){
+		alert ('Attenzione: Le due password non combaciano');
+		return false;
+	}
+		
+	if(psw.length < 6){
+		alert ('Attenzione: La password deve essere lunga almeno 6 caratteri');
+		return false;
+	}
+		
+	if(psw.length > 40){
+		alert ('Attenzione: La password è troppo lunga');
+		return false;
+	}		
 }
