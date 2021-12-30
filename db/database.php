9b605dc4641b6c7dd5c439687346a118f44dcb93
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-12-30 18:16:14 +0100

    small fix riaggiornato a return_object

--- db/database.php
+++ db/database.php
@@ -24,7 +24,7 @@ class DatabaseHelper{
 		$stmt->execute();
         $result = $stmt->get_result();
 
-        return $result->fetch_all(MYSQLI_ASSOC);
+        return $result->fetch_object();
 	}	
 	
 	/* da mettere $amount per prendere una quantità precisa di prodotti */
