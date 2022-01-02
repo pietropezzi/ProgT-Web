1a53d03c8e698c3341018be49ec55d60c20b1608
pietropezzi <pietro.pezzi3@studio.unibo.it>
2022-01-02 09:18:22 +0100

    User info in nav Fixato il problema del testo che usciva su mobile style di userinfo

--- template/user-info.php
+++ template/user-info.php
@@ -1,4 +1,4 @@
-<h3>Attualmente connesso come:</h3>
-<h2><?php echo $_SESSION["nome"]; ?></h2>
-<h4><?php echo $_SESSION["email"]; ?></h4>
-<a href="logout.php">Logout</a>
+<h3 class="mt-2 mx-2 text-white font-verdana">Connesso come:</h3>
+<h2 class="mt-2 mx-2 text-white font-verdana"><?php echo $_SESSION["nome"]; ?></h2>
+<h4 class="mt-2 mx-2 text-white font-verdana"><?php echo $_SESSION["username"]; ?></h4>
+<a class="mt-2 mx-2 font-verdana" href="logout.php">Logout</a>
