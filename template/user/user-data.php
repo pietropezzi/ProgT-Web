e6b58e1ae068a8f81a7ae2da8841691b2ce04348
pietropezzi <pietro.pezzi3@studio.unibo.it>
2022-01-04 09:51:45 +0100

    changes to userdata aggiunto form (parte estetica) aggiorna pass web accessibility per user-info modificata

--- template/user/user-data.php
+++ template/user/user-data.php
@@ -1,8 +1,22 @@
 <div class="userdata">
     <img class="user_image" src="<?php echo USER_IMAGES_DIR.$profile_image; ?>" alt="immagine profilo"/>
-    <h4>Nome:</h4><p><?php echo $profile_data->name; ?></p><br>
-    <h4>Email:</h4><p><?php echo $profile_data->email ?> </p><br>
-    <h4>Username:</h4><p><?php echo $profile_data->username ?></p><br>
-    <h4>Tipo:</h4><p><?php echo $profile_data->type ?></p><br>
-    <h4>Telefono:</h4><p><?php echo $profile_data->phone ?></p>
+    <h2>Nome:</h2><p><?php echo $profile_data->name; ?></p><br>
+    <h2>Email:</h2><p><?php echo $profile_data->email ?> </p><br>
+    <h2>Username:</h2><p><?php echo $profile_data->username ?></p><br>
+    <h2>Tipo:</h2><p><?php echo $profile_data->type ?></p><br>
+    <h2>Telefono:</h2><p><?php echo $profile_data->phone ?></p>
+</div>
+<div class="update_userdata_form">
+    <form>
+        <fieldset>
+            <legend>Aggiorna Password</legend>
+            <p>Per aggiornare la password, reinserire la vecchia password per la convalida per poi inserire la nuova due volte</p>
+            <label for="oldpass">Vecchia password:</label><input type="text" id="oldpass" name="agg_pass"/><br>
+            <label for="newpass">Nuova password:</label><input type="text" id="newpass" name="agg_pass"/><br>
+            <label for="newpass2">Nuova password:</label><input type="text" id="newpass2" name="agg_pass"/>
+        </fieldset>
+        <fieldset>
+            <legend>Aggiorna Dati</legend>
+        </fieldset>
+    </form>
 </div>
--- template/user/user-info.php
+++ template/user/user-info.php
@@ -1,4 +1,4 @@
 <p>Connesso come:</p>
 <a href="profile.php" class="nome"><?php echo $_SESSION["nome"]; ?></a>
-<h3><?php echo $_SESSION["username"]; ?></h3>
+<h2><?php echo $_SESSION["username"]; ?></h2>
 <a href="logout.php">Logout</a>
