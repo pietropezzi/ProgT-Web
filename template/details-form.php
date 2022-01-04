d50718735b5d76b0007002950c57a229f2915b27
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2022-01-04 16:41:15 +0100

    add css to details

--- template/details-form.php
+++ template/details-form.php
@@ -1,8 +1,25 @@
-<div class="">
-    <h2><?php echo $nome; ?></h2>
-    <h3><?php echo $prezzo; ?>€</h3>
-    <p><?php echo $descrizione; ?></p>
-    <p>Tipologia di prodotto: <?php echo $tipo; ?></p>
-    <p>Quantità disponibile: <?php echo $quantita; ?></p>
-    <p>Venditore: <?php echo $venditore; ?></p>
+<div class="details">
+    <h2 class="m-1"><?php echo $nome; ?></h2>
+    <div class="img">
+        <img class="my-2" src="<?php echo IMAGES_DIR; ?>test.jpg" alt="logo"/>
+    </div>
+    <div class="main m-1">        
+        <h4>Prezzo: <?php echo $prezzo; ?>€</h4>
+        <label class="title font-weight-bold">Dettaglio del prodotto</label>
+        <p><?php echo $descrizione; ?></p>
+    </div>
+    <div class="side">         
+        <?php if($quantita = 0){
+            echo "<p class=\"mx-1\">Prodotto non disponibile</p>";
+        }
+        else{
+            echo "<p class=\"mx-1\">Disponibilità immediata</p>";
+            echo "<form action=\"\" method=\"post\">";        
+            echo "<button name =\"nome\" value=\"\" type=\"submit\">Aggiungi al carrello</button>";
+            echo "</form>";
+        }
+            ?>  
+               
+        <p class="mx-1">Venduto da: <?php echo $venditore; ?></p>
+    </div>     
 </div>
