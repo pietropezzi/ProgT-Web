eeba1924ad8514ba96c46d7b327473565951e340
pietropezzi <pietro.pezzi3@studio.unibo.it>
2022-01-04 11:12:02 +0100

    userdata & userdata form

--- template/user/user-data.php
+++ template/user/user-data.php
@@ -8,15 +8,15 @@
 </div>
 <div class="update_userdata_form">
     <form>
-        <fieldset>
-            <legend>Aggiorna Password</legend>
-            <p>Per aggiornare la password, reinserire la vecchia password per la convalida per poi inserire la nuova due volte</p>
-            <label for="oldpass">Vecchia password:</label><input type="text" id="oldpass" name="agg_pass"/><br>
-            <label for="newpass">Nuova password:</label><input type="text" id="newpass" name="agg_pass"/><br>
-            <label for="newpass2">Nuova password:</label><input type="text" id="newpass2" name="agg_pass"/>
-        </fieldset>
-        <fieldset>
-            <legend>Aggiorna Dati</legend>
-        </fieldset>
+        <h2>Aggiorna password</h2>
+        <p>Per aggiornare la password, reinserire la vecchia password per la convalida per poi inserire la nuova due volte</p>
+        <label for="oldpass">Vecchia password:</label><input type="text" id="oldpass" name="agg_pass"/><br>
+        <label for="newpass">Nuova password:</label><input type="text" id="newpass" name="agg_pass"/><br>
+        <label for="newpass2">Nuova password:</label><input type="text" id="newpass2" name="agg_pass"/>
+        <input class="sub_button" type="submit" value="Aggiorna">
+    </form>
+    <form>
+        <h2>Aggiorna dati</h2>
+        <p>Per lasciare i vari dati invariati non compilare il campo.</p>
     </form>
 </div>
