b17bd3ec403d6343a81be0e78966ac91b85103f7
pietropezzi <pietro.pezzi3@studio.unibo.it>
2022-01-25 09:55:57 +0100

    validate fix for prodotto

--- template/products.php
+++ template/products.php
@@ -2,8 +2,8 @@
     <?php foreach($prodotti as $prod): ?>
     <div class="prodotto">
         <img class="immagine_prodotto" src="<?php echo PROD_IMAGES_DIR.$prod["immagine"]; ?>" alt="immagine_prodotto"/>
-        <h3><?php echo $prod["nome"]; ?></h3>
-        <h4><?php echo $prod["prezzo"]; ?>€</h4>
+        <h2><?php echo $prod["nome"]; ?></h2>
+        <h3><?php echo $prod["prezzo"]; ?>€</h3>
         <p><?php echo $prod["breve_descrizione"]; ?></p>
         <form action="details.php" method="post">
             <input type="hidden" name="venditore" value="<?php echo $prod["venditore"]?>"/>
